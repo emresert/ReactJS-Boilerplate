27d8e7758625a5292f9f00f0fc19f4f9b32ead7c
Emre Sert <emre_sert@kocsistem.com.tr>
2021-02-25 15:46:32 +0300

    Envoriment part is completed

--- src/helper/Utils.ts
+++ src/helper/Utils.ts
@@ -1,1 +1,12 @@
+let endPointUrlForApi: string = "";
 
+if (process.env.NODE_ENV == "production") {
+
+    endPointUrlForApi = "production"  //  https://.../V1 etc.
+    
+} else if (process.env.NODE_ENV == "development") {
+
+    endPointUrlForApi = "development" // https://.../V1 etc.
+
+} 
+export {endPointUrlForApi}
--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from "react";
 import ReactDOM from "react-dom";
 import { FaGithub, FaLinkedin, FaSpinner } from "react-icons/fa"
+import {endPointUrlForApi} from "../helper/Utils"
 
 
 const catLogo = require("../assets/images/cat-r.jpg").default
@@ -9,6 +10,8 @@ const catLogoB = require("../assets/images/images.png").default
 type direction = "left" | "right"
 const App: FC = () => {
     const [iconState, setIconState] = useState<direction>("left")
+    const endPoint:string = endPointUrlForApi
+
     return (
         <div className="App">
             <div className="top">
@@ -16,6 +19,7 @@ const App: FC = () => {
             </div>
             <div className="img-part"><img src={catLogo} className={iconState == "left" ? "cat-logo left" : "cat-logo right"} /></div>
             <div className="img-part"><img width="100" height="100" src={catLogoB} /></div>
+            <div>The project started with {endPoint} mode</div>
         </div>
     )
 }
